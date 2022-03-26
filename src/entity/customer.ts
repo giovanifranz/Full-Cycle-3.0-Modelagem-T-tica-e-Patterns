17acc899fdfcc434f2492179216ad396567e4a69
giovanifranz <giovanifranz@gmail.com>
2022-03-26 19:07:23 -0300

    first commit

---
+++ src/entity/customer.ts
@@ -0,0 +1,48 @@
+import Address from "./address";
+
+export default class Customer {
+  _id: string;
+  _name: string;
+  _address!: Address;
+  _active: boolean = true;
+
+  constructor(id: string, name: string) {
+    this._id = id;
+    this._name = name;
+
+    this.validate();
+  }
+
+  validate(): void {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+  }
+
+  changeName(name: string): void {
+    this._name = name;
+    this.validate();
+  }
+
+  activate(): void {
+    if (this._address === undefined) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
+    this._active = true;
+  }
+
+  deActivate(): void {
+    this._active = false;
+  }
+
+  set address(address: Address) {
+    this._address = address;
+  }
+}
+
+const customer: Customer = new Customer("1", "Giovani");
+console.log(customer);
---
+++ src/entity/order_item.ts
@@ -0,0 +1,26 @@
+export default class OrderItem {
+  _id: string;
+  _name: string;
+  _price: number;
+
+  constructor(id: string, name: string, price: number) {
+    this._id = id;
+    this._name = name;
+    this._price = price;
+
+    this.validate();
+  }
+
+  validate(): void {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+    if (this._price === 0) {
+      throw new Error("Price is required");
+    }
+  }
+
+  get price(): number {
+    return this._price;
+  }
+}
