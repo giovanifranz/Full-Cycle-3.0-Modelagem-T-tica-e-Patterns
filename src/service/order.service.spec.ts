100cb1fd5af19d6b43626e61b7df5afaa553f45f
giovanifranz <giovanifranz@gmail.com>
2022-03-27 16:51:59 -0300

    feat: :sparkles: implementação do programa de reward

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,14 +1,23 @@
 import { describe, it, expect } from "@jest/globals";
-import { Order, OrderItem } from "../entity";
+import { Customer, Order, OrderItem } from "../entity";
 import { OrderService } from "./";
 
 describe("Order service unit tests", () => {
-  it("should get total of all orders", () => {
-    const orderItem_1 = new OrderItem("1", "item 1", 100, "Product_1", 1);
-    const orderItem_2 = new OrderItem("2", "item 2", 200, "Product_2", 2);
+  it("should place an order", () => {
+    const customer = new Customer("1", "Giovani Franz");
+    const item_1 = new OrderItem("1", "item 1", 100, "Product_1", 1);
 
-    const order_1 = new Order("1", "Customer_1", [orderItem_1]);
-    const order_2 = new Order("2", "Customer_2", [orderItem_2]);
+    const order = OrderService.placeOrder(customer, [item_1]);
+    expect(customer.rewardPoints).toBe(50);
+    expect(order.total()).toBe(100);
+  });
+
+  it("should get total of all orders", () => {
+    const item_1 = new OrderItem("1", "item 1", 100, "Product_1", 1);
+    const item_2 = new OrderItem("2", "item 2", 200, "Product_2", 2);
+
+    const order_1 = new Order("1", "Customer_1", [item_1]);
+    const order_2 = new Order("2", "Customer_2", [item_2]);
 
     const total = OrderService.total([order_1, order_2]);
     expect(total).toBe(500);
--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,6 +1,16 @@
-import { Order } from "../entity";
-
+import { Customer, Order, OrderItem } from "../entity";
+import { randomUUID } from "crypto";
 export class OrderService {
+  static placeOrder(customer: Customer, items: OrderItem[]): Order {
+    if (items.length === 0) {
+      throw new Error("Order must habe at least one item");
+    }
+
+    const order = new Order(randomUUID(), customer.id, items);
+    customer.addRewardPoints(order.total() / 2);
+    return order;
+  }
+
   static total(orders: Order[]): number {
     return orders.reduce((acc, order) => acc + order.total(), 0);
   }
